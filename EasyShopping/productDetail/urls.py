0156a82f5fde7ce9cf000e557f498a0711febd93
quang nguyen <quangjr07@gmail.com>
2024-10-23 08:52:28 +0700

    up

--- EasyShopping/productDetail/urls.py
+++ EasyShopping/productDetail/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
-from . import views  
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+    # path('show/', views.show, name='show'),
+    # path('')
+#     path('api/product/<int:productID>/', ProductDetailView.as_view(), name='product-detail'),
+# ]
+
+from rest_framework.routers import DefaultRouter
+from .views import *
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
-    path('show/', views.show, name='show'),
-    
+    path('api/', include(router.urls)),  # Include all the routes for the ProductViewSet
+    path('<pid>', productDetailView, name="product-detail"),
 ]
--- EasyShopping/productDetail/views.py
+++ EasyShopping/productDetail/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-def show(request):
-    pass
+# def show(request):
+#     pass
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.views import View
+# import random
+# from core.models import *
+# Create your views here.
+from rest_framework import viewsets
+from core.models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import render
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'productID'  # Retrieve by productID instead of default 'id'
+
+# class ProductDetailView(View):
+    # def get(self, request, **kwargs):
+        # product = models.Product.objects.filter(id = int(kwargs.get('id')))
+        # product = product[0]
+        # relatedProduct = models.Product.objects.filter(category = product.category)
+        # se = set()
+        # for i in range(len(relatedProduct)):
+        #     randIndex = random.randint(0)
+            
+        # print(relatedProduct)
+        # context = {
+        #     "product" : product,
+        #     'message' : "hello"
+        # }
+        # return render(request, 'productdetail/index.html')
+def productDetailView(request, pid):
+    product = Product.objects.get(productID = pid)
+    print(product.items.all())
+    context = {
+        "product" : product
+    }
+    return render(request, 'productDetail/index.html', context)
+    
