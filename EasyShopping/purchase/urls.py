590eb252502406754a90276c2ae967dfce9e309b
PhucLee2304 <miracle23042004@gmail.com>
2024-11-01 00:18:38 +0700

    checkout

--- EasyShopping/purchase/urls.py
+++ EasyShopping/purchase/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.payment, name='payment'),
-    
+    # path('payment/', views.payment, name='payment'),
+    # path('scan/', views.scan, name='scan'),
 ]
