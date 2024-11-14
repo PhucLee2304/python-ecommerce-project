8e67d48de0fc4039b2789f79a39ccdb7aa8b190a
quang nguyen <quangjr07@gmail.com>
2024-11-14 21:51:22 +0700

    chinh lai view product detail

--- EasyShopping/productDetail/urls.py
+++ EasyShopping/productDetail/urls.py
@@ -1,22 +1,9 @@
-# from django.urls import path
-# from .views import *
-
-# urlpatterns = [
-    # path('show/', views.show, name='show'),
-    # path('')
-#     path('api/product/<int:productID>/', ProductDetailView.as_view(), name='product-detail'),
-# ]
-
 from rest_framework.routers import DefaultRouter
 from .views import *
 from django.urls import path, include
 
-# app_name = 'productDetail'
-
-router = DefaultRouter()
-router.register(r'cart-item', CartViewSet, basename='cart-item')
 
 urlpatterns = [
-    path('api/', include(router.urls)),  # Include all the routes for the ProductViewSet
     path('products/<pid>/', productDetailView, name="product-detail"),
+    path('api/cart-item/', add_to_cart),
 ]
